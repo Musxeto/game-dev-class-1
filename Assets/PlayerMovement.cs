8b805634ac24138aa24d9595ec4747f8316a0359
musxeto <mustafamalikawan786@gmail.com>
2025-03-17 09:50:02 +0500

    audio

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,23 +5,34 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
     public float forwardForce = 500f;
     public float sidewayForce = 2000f;
+    public ParticleSystem groundImpactFX;
+    private bool hasLanded = false; 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb.useGravity = true;
     }
-    // Update is called once per frame
+
     void FixedUpdate()
     {
-        rb.AddForce(0,0,forwardForce*Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        if(Input.GetKey("d")){
-            rb.AddForce(sidewayForce*Time.deltaTime,0,0);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0);
         }
-        if(Input.GetKey("a")){
-            rb.AddForce(-sidewayForce*Time.deltaTime,0,0);
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!hasLanded && collision.gameObject.CompareTag("Ground"))
+        {
+            groundImpactFX.Play();
+            hasLanded = true;
         }
     }
 }
-
