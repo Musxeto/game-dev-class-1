b83c542e2bc0cc40196441ca1f13e70bd81692d4
musxeto <mustafamalikawan786@gmail.com>
2025-03-19 07:19:41 +0500

    jumo

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public float sidewayForce = 2000f;
     public ParticleSystem groundImpactFX;
     private bool hasLanded = false; 
+    public bool isGrounded = true;
+    public float jumpForce = 500f;
 
     void Start()
     {
@@ -25,14 +27,28 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
+        if (Input.GetKeyDown("w") && isGrounded) // Jump
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z); 
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            isGrounded = false;
+        }
     }
-
+    
     void OnCollisionEnter(Collision collision)
     {
         if (!hasLanded && collision.gameObject.CompareTag("Ground"))
         {
             groundImpactFX.Play();
             hasLanded = true;
+        }
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; // Allow jumping again
+        }
+        else if (collision.gameObject.CompareTag("Obstacle")) // Game Over
+        {
+            // GameOver();
         }
     }
 }
